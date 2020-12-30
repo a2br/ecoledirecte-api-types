b943632479c562c890ca057a82970355bd755e70
Anatole <62328077+a2br@users.noreply.github.com>
2020-12-30 10:06:04 +0100

    Use of Record for unpredictable keys

--- v3/responses/eleves/cahierdetexte.ts
+++ v3/responses/eleves/cahierdetexte.ts
@@ -70,7 +70,7 @@ export type textbookResSuccess = {
 	 * @example data["2020-01-14"]: Array<textbookAssignement>
 	 */
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	data: any;
+	data: Record<string, Array<textbookAssignement>>;
 };
 
 export type textbookAssignement = {
