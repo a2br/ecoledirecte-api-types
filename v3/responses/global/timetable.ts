519823cedec616b106d2739196225fce7d92c2a0
Clément Boittin <clemorange22@gmail.com>
2021-12-12 19:38:07 +0100

    ✨ Add timetable types

---
+++ v3/responses/global/timetable.ts
@@ -0,0 +1,39 @@
+import { failureRes } from "../failure";
+
+export type timetableRes = timetableResSucess | failureRes;
+
+export type timetableResSucess = {
+    code: 200;
+    token: string;
+    host : string;
+    data: timetableResData;
+};
+
+export type timetableResData = course[];
+
+export type course = {
+    id: number;
+    text: string;
+    matiere: string;
+    codeMatiere: string;
+    typeCours: string;
+    start_date: string;
+    end_date: string;
+    color: string;
+    dispensable: boolean;
+    dispense: number;
+    prof: string;
+    salle: string;
+    classe: string;
+    classeId: number;
+    classeCode: string;
+    groupe: string;
+    groupeCode: string;
+    isFlexible: boolean;
+    groupeId: number;
+    icone: string;
+    isModifie: boolean;
+    contenuDeSeance: boolean;
+    devoirAFaire: boolean;
+    isAnnule: boolean;
+};
